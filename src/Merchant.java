4c4629dba23060e8cfe88071045c7f6004a42b1a
HolovinMaksim <holovinmaksim@gmail.com>
2022-06-03 13:08:50 +0300

    Update class Merchant

--- src/Merchant.java
+++ src/Merchant.java
@@ -1,18 +1,28 @@
 public class Merchant {
 
     private final String name;
+    private final String surname;
     private final String phone;
 
-    public Merchant(String name, String phone) {
+
+    public Merchant(String name, String surname, String phone) {
         this.name = name;
+        this.surname = surname;
         this.phone = phone;
+
     }
 
     public String getName() {
         return name;
     }
 
+    public String getSurname() {
+        return surname;
+    }
+
     public String getPhone() {
         return phone;
     }
+
+
 }
