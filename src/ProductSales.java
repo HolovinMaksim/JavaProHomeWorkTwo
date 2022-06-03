465c917c585ad3cc31a4b8ae6a99257ce9253d2b
HolovinMaksim <holovinmaksim@gmail.com>
2022-06-03 08:58:09 +0300

    Add class ProductSales

---
+++ src/ProductSales.java
@@ -0,0 +1,22 @@
+public class ProductSales extends Product implements Sales {
+
+    int qnty;
+    double price;
+
+    public ProductSales(String name, int qnty, double price) {
+        super(name);
+        this.qnty = qnty;
+        this.price = price;
+    }
+
+    public String infoProduct() {
+        String name = getName();
+        String resultSale = Rounder.roundValue(calcSales(qnty, price));
+        return "Product: " + name + "\nSales (USD): " + resultSale;
+    }
+
+    @Override
+    public double calcSales(int quota, double price) {
+        return quota * price;
+    }
+}
