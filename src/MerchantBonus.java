3d7a7af4e8cfec5bc6e685c60705c8dd302fc26c
HolovinMaksim <holovinmaksim@gmail.com>
2022-06-03 13:13:39 +0300

    Update class MerchantBonus

--- src/MerchantBonus.java
+++ src/MerchantBonus.java
@@ -2,8 +2,8 @@ public class MerchantBonus extends Merchant implements Bonus {
 
     String email;
 
-    public MerchantBonus(String name, String phone, String email) {
-        super(name, phone);
+    public MerchantBonus(String name, String surname, String phone, String email) {
+        super(name, surname, phone);
         this.email = email;
     }
 
@@ -19,8 +19,10 @@ public class MerchantBonus extends Merchant implements Bonus {
             return sales * 5 / 100;
         } else if (sales > 10000 && sales <= 30000) {
             return sales * 10 / 100;
+        } else if (sales > 30000 && sales <= 100000) {
+            return sales * 20 / 100;
         } else {
-            return sales * 15 / 100;
+            return sales * 25 / 100;
         }
     }
 }
